d7a0932aed4cc35657bef06b5652e22ec5353284
Dawn Aldrich <dawnstarr.aldrich@gmail.com>
2018-02-28 09:46:14 -0800

    switching driver to Jose

--- server.js
+++ server.js
@@ -8,13 +8,27 @@ const app = express();
 const PORT = process.env.PORT;
 const CLIENT_URL = process.env.CLIENT_URL;
 
+
 const client = new pg.Client(process.env.DATABASE_URL);
 client.connect();
+//wrote after code review
+client.on('error', err => console.error(err));
 
 app.use(cors());
 
-app.get('/', (req, res) => res.send('Testing 1, 2, 3'));
 
+
+//app.get('/', (req, res) => res.send('Testing 1, 2, 3'));
+//after code review
+
+app.get('api/v1/books', (req, res) => {
+  client.query(`SELECT book_id, title, author, image_url, isbn FROM books;`)
+    .then(results => res.send(results.rows))
+    .catch(console.error);
+});
+
+//after code review
+app.get('*', (req, res) => res.redirect(CLIENT_URL));
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
 
 
