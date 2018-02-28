621a905d1167798a0c6bd98a617c0fe27ac75b72
joereyes0123 <rey0123@rey0123s-MacBook-Pro.local>
2018-02-28 11:58:32 -0800

    finished first section of day1

--- server.js
+++ server.js
@@ -18,10 +18,10 @@ app.use(cors());
 
 
 
-//app.get('/', (req, res) => res.send('Testing 1, 2, 3'));
-//after code review
+// app.get('/', (req, res) => res.send('Testing 1, 2, 3'));
+// after code review
 
-app.get('api/v1/books', (req, res) => {
+app.get('/api/v1/books', (req, res) => {
   client.query(`SELECT book_id, title, author, image_url, isbn FROM books;`)
     .then(results => res.send(results.rows))
     .catch(console.error);
